0e1557be77de9adb998f01d073da2de7e1c31a75
Mohammed Omar Faruq Molla <40476870+Faruq2014@users.noreply.github.com>
2021-12-15 21:24:13 -0500

    developed cucumber report (#3)

--- target/cucumber/report.js
+++ target/cucumber/report.js
@@ -38,21 +38,21 @@ formatter.match({
   "location": "BackgroundTest.appIsOpen()"
 });
 formatter.result({
-  "duration": 9945918900,
+  "duration": 6715109400,
   "status": "passed"
 });
 formatter.match({
   "location": "BackgroundTest.userEnterUserNameAndPassword()"
 });
 formatter.result({
-  "duration": 537056200,
+  "duration": 848161100,
   "status": "passed"
 });
 formatter.match({
   "location": "BackgroundTest.clickOnSubmitButton()"
 });
 formatter.result({
-  "duration": 135745100,
+  "duration": 971594000,
   "status": "passed"
 });
 formatter.scenario({
@@ -97,29 +97,27 @@ formatter.match({
   "location": "BackgroundTest.userClickOnPayementPage()"
 });
 formatter.result({
-  "duration": 7477960000,
-  "status": "passed"
+  "duration": 10101613500,
+  "error_message": "org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {\"method\":\"xpath\",\"selector\":\"(//*[contains(text(),\u0027Payments\u0027)])[3]\"}\n  (Session info: chrome\u003d96.0.4664.93)\nFor documentation on this error, please visit: http://seleniumhq.org/exceptions/no_such_element.html\nBuild info: version: \u00273.14.0\u0027, revision: \u0027aacccce0\u0027, time: \u00272018-08-02T20:19:58.91Z\u0027\nSystem info: host: \u0027DESKTOP-INQUNRP\u0027, ip: \u0027192.168.0.184\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_171\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 96.0.4664.93, chrome: {chromedriverVersion: 96.0.4664.45 (76e4c1bb2ab46..., userDataDir: C:\\Users\\Faruq\\AppData\\Loca...}, goog:chromeOptions: {debuggerAddress: localhost:1518}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:extension:credBlob: true, webauthn:extension:largeBlob: true, webauthn:virtualAuthenticators: true}\nSession ID: 5a61122830042dd6d487a83b20b91427\n*** Element info: {Using\u003dxpath, value\u003d(//*[contains(text(),\u0027Payments\u0027)])[3]}\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(Unknown Source)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(Unknown Source)\r\n\tat java.lang.reflect.Constructor.newInstance(Unknown Source)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:548)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:322)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementByXPath(RemoteWebDriver.java:424)\r\n\tat org.openqa.selenium.By$ByXPath.findElement(By.java:353)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:314)\r\n\tat org.openqa.selenium.support.pagefactory.DefaultElementLocator.findElement(DefaultElementLocator.java:69)\r\n\tat org.openqa.selenium.support.pagefactory.internal.LocatingElementHandler.invoke(LocatingElementHandler.java:38)\r\n\tat com.sun.proxy.$Proxy16.click(Unknown Source)\r\n\tat omecenCucumberProject.DashboardPages.PaymentsPage.paymentLink(PaymentsPage.java:19)\r\n\tat omecenCucumberProject.DashboardTests.BackgroundTest.userClickOnPayementPage(BackgroundTest.java:71)\r\n\tat ✽.Given user click on payement page(Background.feature:19)\r\n",
+  "status": "failed"
 });
 formatter.match({
   "location": "BackgroundTest.userValidatePaymentTitle()"
 });
 formatter.result({
-  "duration": 119067700,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.match({
   "location": "BackgroundTest.userValidatePaymentUrl()"
 });
 formatter.result({
-  "duration": 15608800,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.match({
   "location": "BackgroundTest.userCloseTheApp()"
 });
 formatter.result({
-  "duration": 1044258300,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.background({
   "line": 5,
@@ -147,21 +145,21 @@ formatter.match({
   "location": "BackgroundTest.appIsOpen()"
 });
 formatter.result({
-  "duration": 5336218500,
+  "duration": 5116328400,
   "status": "passed"
 });
 formatter.match({
   "location": "BackgroundTest.userEnterUserNameAndPassword()"
 });
 formatter.result({
-  "duration": 463791500,
+  "duration": 1916442800,
   "status": "passed"
 });
 formatter.match({
   "location": "BackgroundTest.clickOnSubmitButton()"
 });
 formatter.result({
-  "duration": 115128500,
+  "duration": 119860600,
   "status": "passed"
 });
 formatter.scenario({
@@ -202,28 +200,28 @@ formatter.match({
   "location": "BackgroundTest.userClickOnInvoicePage()"
 });
 formatter.result({
-  "duration": 7506273800,
+  "duration": 8865088500,
   "status": "passed"
 });
 formatter.match({
   "location": "BackgroundTest.userValidateTheTitle()"
 });
 formatter.result({
-  "duration": 17968800,
+  "duration": 20201100,
   "status": "passed"
 });
 formatter.match({
   "location": "BackgroundTest.userValidateTheUrl()"
 });
 formatter.result({
-  "duration": 10307400,
+  "duration": 4827000,
   "status": "passed"
 });
 formatter.match({
   "location": "BackgroundTest.userCloseTheApp()"
 });
 formatter.result({
-  "duration": 1589886700,
+  "duration": 822183300,
   "status": "passed"
 });
 formatter.uri("BoundaryLoginTest.feature");
@@ -365,7 +363,7 @@ formatter.match({
   "location": "BoundaryLoginTest.browser_and_application_is_open(String)"
 });
 formatter.result({
-  "duration": 5206038500,
+  "duration": 4863156000,
   "status": "passed"
 });
 formatter.match({
@@ -382,22 +380,23 @@ formatter.match({
   "location": "BoundaryLoginTest.i_enter_and(String,String)"
 });
 formatter.result({
-  "duration": 429687000,
+  "duration": 835263600,
   "status": "passed"
 });
 formatter.match({
   "location": "BoundaryLoginTest.i_click_login_button_for_the_app()"
 });
 formatter.result({
-  "duration": 109900000,
+  "duration": 81312300,
   "status": "passed"
 });
 formatter.match({
   "location": "BoundaryLoginTest.i_validate_login_url_for_the_users()"
 });
 formatter.result({
-  "duration": 931595100,
-  "status": "passed"
+  "duration": 678854200,
+  "error_message": "org.junit.ComparisonFailure: did not match expected:\u003c...ory.omecen.com/login[?username\u003dadmin\u0026password\u003dadmin_test]\u003e but was:\u003c...ory.omecen.com/login[]\u003e\r\n\tat org.junit.Assert.assertEquals(Assert.java:117)\r\n\tat omecenCucumberProject.LoginTests.BoundaryLoginTest.i_validate_login_url_for_the_users(BoundaryLoginTest.java:37)\r\n\tat ✽.Then I validate login url for the users(BoundaryLoginTest.feature:11)\r\n",
+  "status": "failed"
 });
 formatter.scenario({
   "line": 16,
@@ -454,7 +453,7 @@ formatter.match({
   "location": "BoundaryLoginTest.browser_and_application_is_open(String)"
 });
 formatter.result({
-  "duration": 6431171800,
+  "duration": 4806951200,
   "status": "passed"
 });
 formatter.match({
@@ -471,22 +470,23 @@ formatter.match({
   "location": "BoundaryLoginTest.i_enter_and(String,String)"
 });
 formatter.result({
-  "duration": 422992200,
+  "duration": 1329833900,
   "status": "passed"
 });
 formatter.match({
   "location": "BoundaryLoginTest.i_click_login_button_for_the_app()"
 });
 formatter.result({
-  "duration": 158650400,
+  "duration": 84318500,
   "status": "passed"
 });
 formatter.match({
   "location": "BoundaryLoginTest.i_validate_login_url_for_the_users()"
 });
 formatter.result({
-  "duration": 834688900,
-  "status": "passed"
+  "duration": 510492600,
+  "error_message": "org.junit.ComparisonFailure: did not match expected:\u003c...ory.omecen.com/login[?username\u003dadmin\u0026password\u003dadmin_test]\u003e but was:\u003c...ory.omecen.com/login[]\u003e\r\n\tat org.junit.Assert.assertEquals(Assert.java:117)\r\n\tat omecenCucumberProject.LoginTests.BoundaryLoginTest.i_validate_login_url_for_the_users(BoundaryLoginTest.java:37)\r\n\tat ✽.Then I validate login url for the users(BoundaryLoginTest.feature:11)\r\n",
+  "status": "failed"
 });
 formatter.uri("Login.feature");
 formatter.feature({
